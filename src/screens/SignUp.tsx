66b055a20a61d0614e61e222c11dde4abdfc6f5e
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-02 20:51:45 -0300

    password confirmation validation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -20,7 +20,8 @@ type FormDataProps = {
 const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
   email: yup.string().required('Informe o e-mail.').email('E-mail inválido.'),
-  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.')
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup.string().required('Confirme sua senha.').oneOf([yup.ref('password')], 'As senhas não são iguais.')
 });
 
 export function SignUp() {
@@ -114,6 +115,7 @@ export function SignUp() {
                 value={value}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
               />
             )}
           />
