5bcebe010501129b3d75e65fe50b3076b0ed8cf7
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-07 19:36:19 -0300

    attaching token to requests

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
 
+import { storageAuthTokenSave } from "@storage/storageAuthToken";
 import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser'
+
 import { api } from '@services/api'
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -20,13 +22,29 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO);
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
 
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true);
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+      await storageUserSave(userData);
+      await storageAuthTokenSave(token);
+      setUser(userData);
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+
+  }
 
   async function signIn(email: string, password: string) {
     try {
       const { data } = await api.post('/sessions', { email, password });
-      if (data.user) {
+      if (data.user && data.token) {
         setUser(data.user);
-        storageUserSave(data.user);
+        storageUserAndToken(data.user, data.token);
+
       }
     } catch (error) {
       throw error;
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,3 +1,4 @@
-const USER_STORAGE = '@minhacademia:user';
+const USER_STORAGE = '@minhaAcademia:user';
+const AUTH_TOKEN_STORAGE = '@minhaAcademia:token'
 
-export { USER_STORAGE };
+export { USER_STORAGE, AUTH_TOKEN_STORAGE };
