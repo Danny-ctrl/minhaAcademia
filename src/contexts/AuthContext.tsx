270d75f95794368e95cbea7f2511afc2bcbc257c
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-07 17:00:44 -0300

    signOut

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,12 +1,13 @@
 import { ReactNode, createContext, useEffect, useState } from "react";
 
-import { storageUserSave, storageUserGet } from '@storage/storageUser'
+import { storageUserSave, storageUserGet, storageUserRemove } from '@storage/storageUser'
 import { api } from '@services/api'
 import { UserDTO } from "@dtos/UserDTO";
 
 export type AuthContextDataProps = {
   user: UserDTO;
   signIn: (email: string, password: string) => Promise<void>;
+  signOut: () => Promise<void>;
   isLoadingUserStorageData: boolean
 }
 
@@ -33,6 +34,20 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
   }
 
+  async function signOut() {
+    try {
+      setIsLoadingUserStorageData(true);
+      setUser({} as UserDTO);
+      await storageUserRemove();
+
+    } catch (error) {
+      throw error;
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
+
   async function loadUserData() {
     const userLogged = await storageUserGet();
     try {
@@ -56,6 +71,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     <AuthContext.Provider value={{
       user,
       signIn,
+      signOut,
       isLoadingUserStorageData
     }}>
       {children}
