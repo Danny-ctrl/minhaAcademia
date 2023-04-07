d26b82e7e2c4baa59a30637fd9b036234a1cf1c8
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-07 16:32:18 -0300

    displaying registered user name

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,15 @@
 import { TouchableOpacity } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
 import { HStack, VStack, Text, Heading, Icon } from 'native-base';
+
 import { UserPhoto } from './UserPhoto';
+
+import { MaterialIcons } from '@expo/vector-icons';
+import { useAuth } from '@hooks/useAuth';
 
 
 export function HomeHeader() {
+  const { user } = useAuth();
+
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
       <UserPhoto
@@ -18,7 +23,7 @@ export function HomeHeader() {
           Olá,
         </Text>
         <Heading color="gray.100" fontSize="md" fontFamily="heading">
-          Daniele
+          {user.name}
         </Heading>
       </VStack>
 
