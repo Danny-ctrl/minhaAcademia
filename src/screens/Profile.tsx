10662667d920dadeb90af7981ede8c5c1143d302
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-08 04:24:27 -0300

    image upload

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -95,10 +95,23 @@ export function Profile() {
 
 
         const photoFile = {
-          name: `${user.name}.${fileExtension}`.toLocaleLowerCase(),
+          name: `${user.name}.${fileExtension}`.toLowerCase(),
           uri: photoSelected.assets[0].uri,
           type: `${photoSelected.assets[0].type}/${fileExtension}`,
-        }
+        } as any;
+        const userPhotoUploadForm = new FormData();
+        userPhotoUploadForm.append('avatar', photoFile);
+
+        await api.patch('/users/avatar', userPhotoUploadForm, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        toast.show({
+          title: 'Foto atualizada',
+          placement: 'top',
+          bgColor: 'green.500'
+        })
       }
 
     } catch (error) {
