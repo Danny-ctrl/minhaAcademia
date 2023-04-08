3db79d1394dfa0e29ff8c436c52ddf68ba658bfb
Daniele Freitas <71996747+Danny-ctrl@users.noreply.github.com>
2023-04-08 03:27:57 -0300

    settings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,7 +13,7 @@ export function Routes() {
   const { colors } = useTheme();
   const { user, isLoadingUserStorageData } = useAuth();
 
-  console.log("USUARIO LOGADO", user)
+  console.log("USUÁRIO LOGADO", user)
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
 
